346a786a28a4bc1632fe8a1ab52e188d03a5b479
Hotasiii <18219083@std.stei.itb.ac.id>
2023-02-01 18:32:06 +0700

    added extended vigenere, some minor cases WIP

--- Tucil1.py
+++ Tucil1.py
@@ -1,47 +1,63 @@
 # Vigenere Cipher standard (26 huruf alfabet)
-def vigenereStandardEncrypt(plainText: str, key: str) -> str:
+def vigenereStandardKeyGenerator(plainText: str, key: str) -> str:
     # Key handling if len(key) < len(plainText)
     if len(key) < len(plainText):
         key = list(key) # Key harus diubah menjadi list terlebih dahulu agar bisa diappend
         for i in range(len(plainText) - len(key)):
             key.append(key[i % len(key)])  
-        "".join(key)          
 
-    # Encrypt algorithm
+            # Case if key not alphabet
+        return "".join(key)          
+
+def vigenereStandardEncrypt(plainText: str, key: str) -> str:
     cipherText = []
     for i in range(len(plainText)):
+        # Case if plainText or key not alphabet 
+        # Abaikan numerik, spesial karakter, spasi
+        # Kode
+
+        # Main encrypt algorithm
         encrpyt = (ord(plainText[i]) + ord(key[i])) % 26
         encrpyt += ord('A')
         cipherText.append(chr(encrpyt))
-    return print("".join(cipherText))
+    return print("".join(cipherText), key)
+
 
 def vigenereStandardDecrpyt(cipherText: str, key: str) -> str:
     origText = []
+
+    
     # Decrypt algorithm
     for i in range(len(cipherText)):
+
+        # Case if plainText or key not alphabet 
+        # Abaikan numerik, spesial karakter, spasi
+        # Kode
+
+        # Main decrypt algorithm
         decrpyt = (ord(cipherText[i]) - ord(key[i]) + 26) % 26 # Ditambah 26 sebelum di modulo karena modulo negative numbers in Python kind of works differently
         decrpyt += ord('A')
         origText.append(chr(decrpyt))
     return print("".join(origText))
 
 # Extended Vigenere Cipher: Auto-key (256 karakter ASCII)
-def vigenereAutoKeyEncrypt(plainText: str, key: str) -> str:
-    # Kode
-    return 0
-
-def vigenereAutoKeyDecrypt(plainteks: str, key: str) -> str:
-    # Kode
-    return 0
+def vigenereAutoKeyGenerator(plainText: str, key: str) -> str:
+    # Read file as a file of bytes, special characters, numbers, spaces included (256 ASCII)
+    # Extended vigenere cipher: Auto-key Vigenere - Key handling if len(key) > len(plainText)
+    if len(key) > len(plainText):
+        key = list(key)
+        for i in range(len(plainText) - len(key)):
+            key.append(plainText[i % len(plainText)])
+        return "".join(key)
 
 # Playfair Cipher (26 huruf alfabet)
 # One-time pad (26 huruf alfabet)
 # Bonus: Enigma Cipher dengan 3-rotor (26 huruf alfabet)
 
-# Input
+# Input from keyboard
 plainText, key= input(), input()
-# vigenereStandardEncrypt(plainText, key)
-# vigenereStandardDecrpyt(plainText, key)
+vigenereStandardEncrypt(plainText, key)
 
+# Input from file, read file (ex: .txt/word)
 
-
-
+# Simpan cipherteks ke dalam file = not yet done 
