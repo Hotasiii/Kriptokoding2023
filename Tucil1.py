420053da4dd01720ebc8eec0ac77ecf750661518
Hotasiii <18219083@std.stei.itb.ac.id>
2023-02-01 20:01:20 +0700

    bingung wkwwkkw

--- Tucil1.py
+++ Tucil1.py
@@ -1,63 +1,70 @@
 # Vigenere Cipher standard (26 huruf alfabet)
-def vigenereStandardKeyGenerator(plainText: str, key: str) -> str:
-    # Key handling if len(key) < len(plainText)
-    if len(key) < len(plainText):
-        key = list(key) # Key harus diubah menjadi list terlebih dahulu agar bisa diappend
-        for i in range(len(plainText) - len(key)):
-            key.append(key[i % len(key)])  
+def VigenereStandard(plainText: str, key: str) -> str:
+    # Hanya mengambil 26 huruf alfabet 
+    newPlainText = ""
+    newKey = ""
 
-            # Case if key not alphabet
-        return "".join(key)          
-
-def vigenereStandardEncrypt(plainText: str, key: str) -> str:
-    cipherText = []
     for i in range(len(plainText)):
-        # Case if plainText or key not alphabet 
-        # Abaikan numerik, spesial karakter, spasi
-        # Kode
+        if (ord(i) >= 65 and ord (i) <= 122):
+            newPlainText += i 
 
-        # Main encrypt algorithm
-        encrpyt = (ord(plainText[i]) + ord(key[i])) % 26
-        encrpyt += ord('A')
-        cipherText.append(chr(encrpyt))
-    return print("".join(cipherText), key)
-
-
-def vigenereStandardDecrpyt(cipherText: str, key: str) -> str:
-    origText = []
-
+    for j in range(len(key)):
+        if (ord(j) >= 65 and ord(i) <= 122):
+            newKey += j 
     
-    # Decrypt algorithm
+    # Standardizing all plainText and Key to uppercase
+    newPlainText = newPlainText.upper()
+    newKey = newKey.upper()
+
+    # Key handling if len(key) < len(plainText)
+    if len(newKey) < len(newPlainText):
+        key = list(newKey) # Key harus diubah menjadi list terlebih dahulu agar bisa diappend
+        for i in range(len(newPlainText) - len(newKey)):
+            newKey.append(newKey[i % len(newKey)])  
+            newKey = "".join(newKey)
+
+    # Main encrypt algorithm       
+    result = []
+    for i in range(len(newPlainText)):
+        cipherText = ((ord(newPlainText[i]) + ord(newKey[i])) % 26) + ord('A')
+        result.append(chr(cipherText))
+    result = "".join(result)
+
+    # Main decrypt algorithm
+    result = []
     for i in range(len(cipherText)):
+        origText = ((ord(cipherText[i]) - ord(newKey[i]) + 26) % 26) + ord('A') # Ditambah 26 sebelum di modulo karena modulo negative numbers in Python kind of works differently
+        result.append(chr(origText))
+    result = "".join(result)
 
-        # Case if plainText or key not alphabet 
-        # Abaikan numerik, spesial karakter, spasi
-        # Kode
-
-        # Main decrypt algorithm
-        decrpyt = (ord(cipherText[i]) - ord(key[i]) + 26) % 26 # Ditambah 26 sebelum di modulo karena modulo negative numbers in Python kind of works differently
-        decrpyt += ord('A')
-        origText.append(chr(decrpyt))
-    return print("".join(origText))
+    return result
 
 # Extended Vigenere Cipher: Auto-key (256 karakter ASCII)
-def vigenereAutoKeyGenerator(plainText: str, key: str) -> str:
-    # Read file as a file of bytes, special characters, numbers, spaces included (256 ASCII)
-    # Extended vigenere cipher: Auto-key Vigenere - Key handling if len(key) > len(plainText)
-    if len(key) > len(plainText):
-        key = list(key)
-        for i in range(len(plainText) - len(key)):
-            key.append(plainText[i % len(plainText)])
-        return "".join(key)
+def vigenereExtendedEncrypt(plain, key: bytes):
+    result = ""
 
-# Playfair Cipher (26 huruf alfabet)
-# One-time pad (26 huruf alfabet)
-# Bonus: Enigma Cipher dengan 3-rotor (26 huruf alfabet)
+    for i in range(len(plain)):
+        if ord(plain[i] >= 0 and ord(plain[i]) <= 255):
+            cipherText = (ord(plain[i] + ord(key[i % len(key)]))) % 256
+            result += chr(cipherText)
+        else:
+            result += plain[i]    
+    return result
+
+def vigenereExtendedEncrypt(plain, key: bytes):
+    result = ""
+
+    for i in range(len(plain)):
+        if ord(plain[i] >= 0 and ord(plain[i]) <= 255):
+            origText = (ord(plain[i] - ord(key[i % len(key)]))) % 256
+            result += chr(origText)
+        else:
+            result += plain[i]    
+    return result 
+
 
 # Input from keyboard
 plainText, key= input(), input()
-vigenereStandardEncrypt(plainText, key)
-
 # Input from file, read file (ex: .txt/word)
 
 # Simpan cipherteks ke dalam file = not yet done 
